c3bbd108f4dd4497091e0549451469390b39161e
duh-git <cherebeevkarim2015@gmail.com>
2026-07-19 19:56:47 +0300

    hero block

--- stash/main.new.js
+++ stash/main.new.js
@@ -1,37 +1,35 @@
-// APP_SCRIPT_URL = "asd"
 const APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw7NcguDr3Hx646jtX_5KvBZudmTkESZZpbZ8fODZYf-WRdsfHLpEJ2V0Ebu5tmttM/exec"
 
 
-// window.addEventListener('scroll', () => {
-//   const headerClassList = document.querySelector('header').classList;
-//   window.scrollY > 400 ? headerClassList.add("sticky") : headerClassList.remove("sticky");
-// });
-
-
 // Отправка данных
-const forms = document.querySelectorAll('form');
+document.querySelectorAll('form')
+  .forEach(form => {
+    form.addEventListener('submit', async (event) => {
+      event.preventDefault();
 
-forms.forEach(form => {
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
+      const resultOutput = form.querySelector(".result");
+      resultOutput.textContent = 'Отправка...';
 
-    const resultOutput = form.querySelector(".result");
-    resultOutput.textContent = 'Отправка...';
+      try {
+        const formData = new FormData(form);
+        // const response = await fetch(APP_SCRIPT_URL, {
+        //   method: 'POST',
+        //   body: formData
+        // });
 
-    try {
-      const formData = new FormData(form);
-      // const response = await fetch(APP_SCRIPT_URL, {
-      //   method: 'POST',
-      //   body: formData
-      // });
-
-      if (response.ok) {
-        resultOutput.textContent = 'Заявка успешно отправлена!';
-      } else {
-        resultOutput.textContent = 'Ошибка сервера. Попробуйте позже.';
+        if (response.ok) {
+          resultOutput.textContent = 'Заявка успешно отправлена!';
+        } else {
+          resultOutput.textContent = 'Ошибка сервера. Попробуйте позже.';
+        }
+      } catch (error) {
+        resultOutput.textContent = 'Ошибка сети. Проверьте соединение.';
       }
-    } catch (error) {
-      resultOutput.textContent = 'Ошибка сети. Проверьте соединение.';
-    }
-  });
-})
+    });
+  })
+
+function toggleHeaderMenu() {
+  document.querySelectorAll("header nav, header .contacts").forEach(el => el.toggleAttribute("open"))
+}
+
+document.querySelectorAll("header nav a").forEach(el => el.addEventListener("click", toggleHeaderMenu))
